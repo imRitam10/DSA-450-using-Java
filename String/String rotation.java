5725e375acf70e73645f866002f222eb2a6b00a7
Ritam Bhadra <imritam29@gmail.com>
2021-08-05 20:41:53 +0530

    A Program to check if strings are rotations of each other or not

---
+++ String/String rotation.java
@@ -0,0 +1,8 @@
+
+public class RotationOfString {
+    static boolean areRotations(String str1, String str2){
+        // There lengths must be same and str2 must be
+        // a substring of str1 concatenated with str1.
+        return (str1.length() == str2.length()) && ((str1 + str1).contains(str2));
+    }
+}
