e7bb6b1fa19e3369c7a4bb94c97d18d140ee32d0
Ritam Bhadra <imritam29@gmail.com>
2021-08-22 21:09:07 +0530

    Searching in an array where adjacent differ by at most k

---
+++ Searching & Sorting/Searching in an array.java
@@ -0,0 +1,11 @@
+class solution{
+	static int search(int arr[], int n, int x, int k){
+		int i = 0;
+		while(i < n){
+			if(arr[i] == x) return i;
+					
+			i = i + Math.max(1, Math.abs(arr[i] - x) / k);
+		}
+		return -1;
+	}
+}
