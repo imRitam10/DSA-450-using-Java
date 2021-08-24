b13f6900dc6a6e109665ff8c48aff4266c3d76ce
Ritam Bhadra <imritam29@gmail.com>
2021-08-24 20:41:23 +0530

    Count triplet with sum smaller than a given value

---
+++ Searching & Sorting/Count triplets with sum smaller than X.java
@@ -0,0 +1,22 @@
+
+class Solution{
+    long countTriplets(long arr[], int n,int sum){
+        Arrays.sort(arr);
+        long res = 0;
+        for(int i = 0 ; i < n -2 ; i++){
+            
+            int j = i + 1;
+            int k = n - 1;
+            
+            while(j < k){
+                if(arr[i] + arr[j] + arr[k] >= sum) k--;
+                
+                else {
+                    res += (k - j);
+                    j++;
+                }
+            }
+        }
+        return res;
+    }
+}
