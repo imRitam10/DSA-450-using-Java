0a4dc76bcd407ac9c3cd29d50597d96f82bb6114
Ritam Bhadra <imritam29@gmail.com>
2021-08-26 10:50:07 +0530

    K-th element of two sorted Arrays using BS && PriorityQueue

---
+++ Searching & Sorting/K-th element of two sorted Arrays.java
@@ -0,0 +1,61 @@
+/*	
+	Using PriorityQueue->
+	
+	Push the elements of both arrays to a priority queue (min-heap).
+	Pop-out k-1 elements from the front.
+	Element at the front of the priority queue is the required answer.
+	
+*/
+
+
+class Solution {
+    public long kthElement( int arr1[], int arr2[], int n, int m, int k) {
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < n; i++)
+            pq.offer(arr1[i]);
+
+        for (int i = 0; i < m; i++) 
+            pq.offer(arr2[i]);
+            
+        while (k-- > 1) {
+            pq.remove();
+        }
+        return pq.peek();
+    }
+}
+
+
+
+/* 
+	Most efficient using BS 
+	TC -> O(log(min(n,m)))
+	https://www.youtube.com/watch?v=nv7F4PiLUzo
+	
+*/
+	
+class Solution {
+    public long kthElement( int arr1[], int arr2[], int n, int m, int k) {
+        if(n > m)
+            return kthElement(arr2, arr1, m, n, k); 
+        
+        int low = Math.max(0,k-m), high = Math.min(k,n);
+        
+        while(low <= high) {
+            
+            int cut1 = (low + high) / 2; 
+            int cut2 = k - cut1;
+            
+            int l1 = cut1 == 0 ? Integer.MIN_VALUE : arr1[cut1 - 1]; 
+            int l2 = cut2 == 0 ? Integer.MIN_VALUE : arr2[cut2 - 1];
+            int r1 = cut1 == n ? Integer.MAX_VALUE : arr1[cut1]; 
+            int r2 = cut2 == m ? Integer.MAX_VALUE : arr2[cut2]; 
+            
+            if(l1 <= r2 && l2 <= r1) return Math.max(l1, l2);
+            else if (l1 > r2) high = cut1 - 1;
+            else low = cut1 + 1; 
+        }
+        return 1; 
+    }
+}
