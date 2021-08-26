5b89fb526b53929ffa4b432588fbee8046c59091
Ritam Bhadra <imritam29@gmail.com>
2021-08-26 19:45:04 +0530

    Missing Number in AP

---
+++ Searching & Sorting/Arithmetic Number.java
@@ -0,0 +1,18 @@
+class Solution{
+    static int inSequence(int A, int B, int C){
+    // code here
+        if( C == 0 ){
+        if( B==A) return 1;
+            return 0 ;
+        }
+        int d = (B-A)%C;
+        if( d == 0 ){
+        
+            int k = 1+ (B-A)/C;
+            if( k >=1){
+            return 1 ;
+            }
+        }
+        return 0 ;
+    }
+}
