b285f97716a9bedc124a61d6889e51e8848c2fde
Ritam Bhadra <imritam29@gmail.com>
2021-09-08 20:01:06 +0530

    Linked List Cycle II

---
+++ LinkedList/Linked List Cycle II.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head){
+        
+        if(head == null || head.next == null) return null;
+        
+        ListNode slow = head, fast = head, c = head;
+
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(fast == slow){
+                while(slow != c){
+                    c = c.next ;
+                    slow = slow.next ;                    
+                }
+                return c;
+            }
+        }
+        return null;  
+    }
+}
